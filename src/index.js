51b2959729781d204df76f8dc67ff96907f4f86e
Shiva Shrestha <shivashrestha927@gmail.com>
2024-12-03 13:28:08 +0545

    f

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/portfolio" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <App />
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<App />} />
+  //     <Route path="/portfolio" element={<App />} />
+  //   </Routes>
+  // </BrowserRouter>
 );
